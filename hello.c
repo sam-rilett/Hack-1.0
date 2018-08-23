24579c62e2c0e2e80cc2078369f277e1528fbb82
sam-rilett <sam.rilett@gmail.com>
2018-08-23 13:09:57 -0500

    Hack 1.0
    Basics of Git

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author:Sam Rilett
+ * Date: 8/23/2018
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Sam Rilett, Computer Science Major\n");
+
+  return 0;
+}
